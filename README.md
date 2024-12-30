4e208ac7dfc5314a79af5d0722c2a6987cac828f
Habiba Rahman <habiba@glascutr.com>
2024-12-30 12:07:17 +0600

    Sync App Completed

---
+++ README.md
@@ -0,0 +1,7 @@
+## Attendance Sync
+
+A robust integration system that connects attendance devices to automate log fetching, employee mapping, and real-time attendance updates. It ensures accuracy, prevents duplicates, and streamlines HR attendance processes.
+
+#### License
+
+agpl-3.0
---
+++ attendance_sync/attendance_sync/doctype/device_configuration/test_device_configuration.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2024, GLASCUTR Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class TestDeviceConfiguration(UnitTestCase):
+	"""
+	Unit tests for DeviceConfiguration.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class TestDeviceConfiguration(IntegrationTestCase):
+	"""
+	Integration tests for DeviceConfiguration.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
